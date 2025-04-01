15f870b359c4390c7272f0e486c39dd65c685916
Macmilan24 <cpt.macmilan24@gmail.com>
2025-04-01 18:44:04 +0300

    Create: Post request endpoint

--- src/chatbot.py
+++ src/chatbot.py
@@ -1,1 +1,15 @@
+from llm_api import get_groq_response
 
+
+def get_chat_reponse(user_input: str) -> str:
+    """
+    Accepts user input and get the response from Groq API
+
+    Args:
+        user_input (str): user input
+
+
+    Returns:
+        str: Response from Groq API
+    """
+    return get_groq_response(user_input)
--- src/main.py
+++ src/main.py
@@ -1,1 +1,19 @@
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+from chatbot import get_chat_reponse
 
+
+class Chat_Request(BaseModel):
+    user_input: str
+
+
+app = FastAPI()
+
+
+@app.post("/chat")
+async def chat_with_ai(request: Chat_Request):
+    try:
+        response = get_chat_reponse(request.user_input)
+        return {"assitance": response}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
